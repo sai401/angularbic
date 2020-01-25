e30b698655db67a9d1426de4bbd6918f58f23060
sai kumar <dugganasaikumar@gmail.com>
2020-01-25 17:01:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './component/test/test.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/test/test.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "app-test",
+  templateUrl: "./test.component.html",
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+  public name = "Jhon";
+  public url = window.location.href;
+  public myId = "testId";
+  public isDisabled = true;
+  public successClass = "text-success";
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+
+  public highlightColor = "orange";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
+  }
+
+  public greeting = "";
+
+  //directives section 
+  public displayName = false;
+  public color = "red";
+  public colors = ["red", "blue", "green", "yellow"]
+  //end
+
+  //components interaction 
+  @Input('parentData') public parentName;
+  @Output() public childEvent = new EventEmitter<string>();
+  public person = {
+    "firstName": "John",
+    "lastName": "Doe"
+  }
+
+  public date = new Date();
+  fireEvent() {
+    this.childEvent.emit('Hey Codevolution');
+  }
+
+  //end
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  greetUser() {
+    return "Hello " + this.name;
+  }
+
+  onClick(event) {
+    console.log(event)
+    this.greeting = event.type;
+  }
+
+  logMessage(value) {
+    console.log(value)
+  }
+}
+
+
+
+// selector: ".app-test",
+// selector: '[app-test]',
+
+
+// templateUrl: `
+// <div class="container">
+//   <h2>Card Header and Footer</h2>
+//   <div class="card">
+//     <div class="card-header">Header</div>
+//     <div class="card-body">Content</div> 
+//     <div class="card-footer">Footer</div>
+//   </div>
+// </div>
+// `,
+
+
+// styles: [
+//   `
+//   div{
+//     color:red
+//   }`
+// ]
